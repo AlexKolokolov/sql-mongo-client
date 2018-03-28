9018a6116aa343a2b6e859784883bd03d63cf74c
kolokolov <kolokolov_a@ukr.net>
2018-03-28 18:28:09 +0300

    Add tests for SqlQueryParser

---
+++ src/test/java/org/kolokolov/testtask/queryparser/SqlQueryParserLimitTest.java
@@ -0,0 +1,43 @@
+package org.kolokolov.testtask.queryparser;
+
+import net.sf.jsqlparser.statement.select.Select;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.kolokolov.testtask.Application;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class)
+public class SqlQueryParserLimitTest {
+
+    @Autowired
+    private SqlQueryParser sqlQueryParser;
+
+    @Test
+    public void testLimitWithOffset() {
+        String sqlQuery = "SELECT * FROM x LIMIT 1, 1";
+        Select select = sqlQueryParser.parseSqlQuery(sqlQuery);
+        assertThat(sqlQueryParser.getLimit(select).getOffset()).isEqualTo(1);
+        assertThat(sqlQueryParser.getLimit(select).getRowCount()).isEqualTo(1);
+    }
+
+    @Test
+    public void testLimitWithOutOffset() {
+        String sqlQuery = "SELECT * FROM x LIMIT 1";
+        Select select = sqlQueryParser.parseSqlQuery(sqlQuery);
+        assertThat(sqlQueryParser.getLimit(select).getOffset()).isEqualTo(0);
+        assertThat(sqlQueryParser.getLimit(select).getRowCount()).isEqualTo(1);
+    }
+
+    @Test
+    public void testQueryWithOutLimit() {
+        String sqlQuery = "SELECT * FROM x";
+        Select select = sqlQueryParser.parseSqlQuery(sqlQuery);
+        assertThat(sqlQueryParser.getLimit(select)).isNull();
+    }
+}
---
+++ src/test/java/org/kolokolov/testtask/queryparser/SqlQueryParserOrderByTest.java
@@ -0,0 +1,44 @@
+package org.kolokolov.testtask.queryparser;
+
+import net.sf.jsqlparser.statement.select.Select;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.kolokolov.testtask.Application;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class)
+public class SqlQueryParserOrderByTest {
+
+    @Autowired
+    private SqlQueryParser sqlQueryParser;
+
+    @Test
+    public void testOrderByAscend() {
+        String sqlQuery = "SELECT * FROM x ORDER BY y";
+        Select select = sqlQueryParser.parseSqlQuery(sqlQuery);
+        assertThat(sqlQueryParser.getOrderByElements(select).get(0).getExpression().toString())
+                .isEqualTo("y");
+        assertThat(sqlQueryParser.getOrderByElements(select).get(0).isAsc()).isTrue();
+    }
+
+    @Test
+    public void testOrderByDescend() {
+        String sqlQuery = "SELECT * FROM x ORDER BY y DESC";
+        Select select = sqlQueryParser.parseSqlQuery(sqlQuery);
+        assertThat(sqlQueryParser.getOrderByElements(select).get(0).getExpression().toString())
+                .isEqualTo("y");
+        assertThat(sqlQueryParser.getOrderByElements(select).get(0).isAsc()).isFalse();
+    }
+
+    @Test
+    public void testNoOrderBy() {
+        String sqlQuery = "SELECT * FROM x";
+        Select select = sqlQueryParser.parseSqlQuery(sqlQuery);
+        assertThat(sqlQueryParser.getOrderByElements(select)).isNull();
+    }
+}
