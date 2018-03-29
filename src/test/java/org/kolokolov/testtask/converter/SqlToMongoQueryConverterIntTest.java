2af822e381490075baf73b0f0f964a352ed5b7a3
kolokolov <kolokolov_a@ukr.net>
2018-03-29 21:37:42 +0300

    Add more integration tests

--- src/test/java/org/kolokolov/testtask/converter/SqlToMongoQueryConverterIntTest.java
+++ src/test/java/org/kolokolov/testtask/converter/SqlToMongoQueryConverterIntTest.java
@@ -16,6 +16,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
@@ -53,16 +54,60 @@ public class SqlToMongoQueryConverterIntTest {
     private SqlToMongoQueryConverter converter;
 
     @Test
-    public void prepareQueriesTest() {
+    public void prepareQueriesSingleDocumentTest() {
         Document document = new Document("name", "Bob");
 
         collection.insertOne(document);
 
-        List<FindIterable<Document>> queries = converter.prepareQueries("select * from user where name = 'Bob'");
-        List<Document> documents = queries.stream()
-                .flatMap(query -> query.into(new ArrayList<>()).stream())
-                .collect(toList());
+        List<FindIterable<Document>> queries = converter.prepareQueries("SELECT * FROM user WHERE name = 'Bob'");
+        List<Document> documents = runQueriesAndReturnDocuments(queries);
         assertThat(documents).isNotEmpty();
         assertThat(documents.get(0).get("name")).isEqualTo("Bob");
+    }
+
+    @Test
+    public void prepareQueriesMultipleDocumentsWithOrderByAndLimitTest() {
+        Document document1 = new Document("name", "Bob").append("age", "30");
+        Document document2 = new Document("name", "Dug").append("age", "20");
+        Document document3 = new Document("name", "Kit").append("age", "50");
+
+        collection.insertMany(Arrays.asList(document1, document2, document3));
+
+        String sqlQuery = "SELECT name FROM user WHERE age >= 30 ORDER BY age DESC LIMIT 1, 1";
+
+        List<FindIterable<Document>> queries = converter.prepareQueries(sqlQuery);
+        List<Document> documents = runQueriesAndReturnDocuments(queries);
+        assertThat(documents).isNotEmpty();
+        assertThat(documents).hasSize(1);
+        assertThat(documents.get(0).get("name")).isEqualTo("Bob");
+        assertThat(documents.get(0).get("age")).isNull();
+        assertThat(documents.get(0).get("_id")).isNull();
+    }
+
+    @Test
+    public void prepareQueriesSingleDocumentWithNestedDocumentTest() {
+        Document document = new Document("name", "Bob")
+                .append("contacts", new Document("phone", "555-55-55").append("email", "bob@mail.com"));
+
+        collection.insertOne(document);
+
+        String sqlQuery = "SELECT name, contacts.phone FROM user";
+
+        List<FindIterable<Document>> queries = converter.prepareQueries(sqlQuery);
+        List<Document> documents = runQueriesAndReturnDocuments(queries);
+        assertThat(documents).isNotEmpty();
+        assertThat(documents).hasSize(1);
+        assertThat(documents.get(0).get("name")).isEqualTo("Bob");
+        assertThat(documents.get(0).get("_id")).isNull();
+        assertThat(documents.get(0).get("contacts")).isInstanceOf(Document.class);
+        assertThat(((Document) documents.get(0).get("contacts")).get("phone")).isEqualTo("555-55-55");
+        assertThat(((Document) documents.get(0).get("contacts")).get("email")).isNull();
+
+    }
+
+    private List<Document> runQueriesAndReturnDocuments(List<FindIterable<Document>> queries) {
+        return queries.stream()
+            .flatMap(query -> query.into(new ArrayList<>()).stream())
+            .collect(toList());
     }
 }
