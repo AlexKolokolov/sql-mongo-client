b328da18a6666655e8f114ab90e1b75b761356c4
kolokolov <kolokolov_a@ukr.net>
2018-03-29 20:52:51 +0300

    Replace testdb with devdb

--- runindocker.sh
+++ runindocker.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 read -p 'MongoDB host (default localhost): ' host
 read -p 'MongoDB port (default 27017): ' port
-read -p 'MongoDB database (default testdb): ' database
+read -p 'MongoDB database (default devdb): ' database
 
 [[ -z "$host" ]] && host=localhost
 [[ -z "$port" ]] && port=27017
-[[ -z "$database" ]] && database=testdb
+[[ -z "$database" ]] && database=devdb
 
 docker run -it --rm --net="host" alexkolokolov/sql-mongo-client --mongo.host=$host --mongo.port=$port --mongo.database=$database
